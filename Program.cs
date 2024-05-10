e58a860c1abe195c6e0cb1f87355f88820f2485e
l-fraga2811 <lucasbelter2805@gmail.com>
2024-05-10 19:22:20 -0300

    desafio POO concluido - DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "tijolão", imei: "010101", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+nokia.ReceberLigacao();
+
+Console.WriteLine("iPhone: ");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "de botao", imei: "260385", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Photos");
+iphone.ReceberLigacao();
